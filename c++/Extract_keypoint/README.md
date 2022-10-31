3dac54cba275b73bd06808ac9b7c4d16db289a3d
trebladev <2253714301@qq.com>
2022-10-31 08:15:30 +0800

    feat: Add some comment, and update README.md

---
+++ c++/Extract_keypoint/README.md
@@ -0,0 +1,3 @@
+# A program of extract keypoint
+
+This program is use for extractor image, input a image, show the keypoint in the image
--- c++/Extract_keypoint/main.cpp
+++ c++/Extract_keypoint/main.cpp
@@ -13,6 +13,7 @@ int main(int argc,char **argv){
   argparse::ArgumentParser program("extractor_keypoint");
   program.add_argument("-i", "--input").help("input image");
 
+  //! Set parameters
   try {program.parse_args(argc, argv);}
   catch (const std::runtime_error& err) {std::cerr << err.what() << std::endl << program; exit(1);}
   std::string img_path;
@@ -20,14 +21,18 @@ int main(int argc,char **argv){
     img_path = program.get<std::string>("--input");
 
 
-  printf("input path = %s",img_path.c_str());
+//  printf("input path = %s",img_path.c_str());
   // Read input image
   cv::Mat input_img = cv::imread(img_path);
 
+  // Vector of keypoint and descriptor
   std::vector<cv::KeyPoint> allkeypoint;
   cv::Mat alldesc;
 
+  // RGB image convert to gray image
   cv::cvtColor(input_img,input_img,CV_RGBA2GRAY);
+
+  //! A object of ORBextractor
   ORB_SLAM2::ORBextractor* test_extoractor = new ORB_SLAM2::ORBextractor(
       1000,
       1.2,
@@ -36,6 +41,7 @@ int main(int argc,char **argv){
       8
   );
 
+  //! Extract keypoint
   (*test_extoractor)(
         input_img,
         cv::Mat(),
@@ -47,7 +53,5 @@ int main(int argc,char **argv){
   cv::imshow("input_img",input_img);
   cv::waitKey(0);
 
-
-//  printf("hello world");
   return 0;
 }
